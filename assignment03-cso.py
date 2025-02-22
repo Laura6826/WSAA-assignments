93faa2435865748552b3a8516c990554cf4c2bac
Laura Lyons <maxwell6826@yahoo.co.uk>
2025-02-22 09:42:39 +0000

    Assignment 03 start

---
+++ assignment03-cso.py
@@ -0,0 +1,7 @@
+# Weekly Assignment 03. cso.py
+# The aim of this program is to write a program that retrieves the dataset for the "exchequer account (historical series)" from the CSO, and stores it into a file called "cso.json".
+# Author: Laura Lyons
+
+import requests
+import json
+
