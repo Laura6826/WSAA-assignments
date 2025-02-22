63c9c69641acaad6b67975cae69dd2cd2bf08163
Laura Lyons <maxwell6826@yahoo.co.uk>
2025-02-22 10:29:03 +0000

    Assignment 3-cso

--- assignment03-cso.py
+++ assignment03-cso.py
@@ -5,3 +5,23 @@
 import requests
 import json
 
+# url = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/FIQ02/JSON-stat/2.0/en"
+# response = requests.get(url)
+# data = response.json()
+
+urlBegining = "https://ws.cso.ie/public/api.restful/PxStat.Data.Cube_API.ReadDataset/"
+urlEnd = "/JSON-stat/2.0/en"
+
+def getAllAsFile(dataset):
+    with open("cso.json", "wt") as fp:
+        print(json.dumps(getAll(dataset)), file=fp)
+
+def getAll(dataset):   
+    url = urlBegining + dataset + urlEnd
+    response = requests.get(url)
+    return response.json()
+    
+if __name__ == "__main__":
+    getAllAsFile("FIQ02")
+
+print("Dataset has been saved to 'cso.json'")
